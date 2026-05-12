549fd7a8fd071b50e618379180e9c8e4c20a039d
942021958 <50169471+942021958@users.noreply.github.com>
2026-05-12 10:26:28 +0800

    Delete plugins/LittleWhiteBox-2.6.2 directory

--- plugins/LittleWhiteBox-2.6.2/core/constants.js
+++
@@ -1,7 +0,0 @@
-/**
- * LittleWhiteBox 共享常量
- */
-
-export const EXT_ID = "LittleWhiteBox";
-export const EXT_NAME = "小白X";
-export const extensionFolderPath = `scripts/extensions/third-party/${EXT_ID}`;
